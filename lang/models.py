d7f1f5c247b8aa433b1138f074948fc84a1ddeb3
Slavik Semenenko <slavik.semenenko.ua@gmail.com>
2024-01-27 13:07:04 -0500

    improve model

--- lang/models.py
+++ lang/models.py
@@ -5,9 +5,10 @@ from django.utils.translation import gettext_lazy as _
 class WordModel(models.Model):
     class PassedStep(models.IntegerChoices):
         NO_PASS = 0, _("no passed")
-        DAY_3TH = 1, _("3th day")
-        DAY_7TH = 2, _("7th day")
-        DAY_30TH = 3, _("30th day")
+        DAY_1TH = 1, _("1th day")
+        DAY_3TH = 3, _("3th day")
+        DAY_7TH = 7, _("7th day")
+        DAY_30TH = 30, _("30th day")
 
     target_phrase = models.CharField(max_length=200)
     translated_phrase = models.CharField(max_length=200)
